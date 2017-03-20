44002ae7736481ce17b741e80249847867c77e2c
KaloyanBogoslovov2 <kaloyan_bogoslovov@yahoo.com>
2017-03-20 17:21:51 +0200

    added image saving

--- app/src/main/java/com/bogoslovov/kaloqn/drawing/MainActivity.java
+++ app/src/main/java/com/bogoslovov/kaloqn/drawing/MainActivity.java
@@ -1,11 +1,22 @@
 package com.bogoslovov.kaloqn.drawing;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.UUID;
+
+import static com.bogoslovov.kaloqn.drawing.R.id.drawing;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,22 +26,37 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int LINE_THIN = 5;
     private static final int LINE_THICK = 15;
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE=0;
 
-    private DrawingView drawingView;
+    public static DrawingView drawingView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initDrawingView();
+        checkForPermissions();
     }
 
     private void initDrawingView(){
 
         Paint paint = getPaint();
         drawingView = new DrawingView(this,paint);
-        LinearLayout layout = (LinearLayout) findViewById(R.id.drawing);
+        drawingView.setDrawingCacheEnabled(true);
+        LinearLayout layout = (LinearLayout) findViewById(drawing);
         layout.addView(drawingView);
+    }
+
+    private void checkForPermissions(){
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+        }
     }
 
     private Paint getPaint(){
@@ -43,6 +69,18 @@ public class MainActivity extends AppCompatActivity {
         paint.setStrokeCap(Paint.Cap.ROUND);
         paint.setStrokeWidth(LINE_THIN);
         return paint;
+    }
+
+    public void saveImage(View v){
+
+        Intent intent = new Intent(MainActivity.this,SaveImageActivity.class);
+        startActivity(intent);
+
+
+    }
+
+    public void openImage(View v){
+
     }
 
     public void changeDrawingLineToThin(View v){
@@ -69,4 +107,5 @@ public class MainActivity extends AppCompatActivity {
     public void changeDrawingColorToRed(View v){
         drawingView.mPaint.setColor(COLOR_RED);
     }
+
 }
---
+++ app/src/main/java/com/bogoslovov/kaloqn/drawing/OpenImageActivity.java
@@ -0,0 +1,18 @@
+package com.bogoslovov.kaloqn.drawing;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kaloqn on 3/20/17.
+ */
+
+public class OpenImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_image);
+    }
+}
